53eac92f41afd99789fa0e023421c25789b2f7b0
White-Chewbacca <bsanoffwiener@gmail.com>
2025-01-30 08:46:47 -0500

    Assignment 1

---
+++ Assignment 1/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int randomNum = (int) (Math.random() * 10) + 1;
+        System.out.println("Please guess a number.");
+        System.out.println(randomNum);
+        int guess = kb.nextInt();
+        while (guess != randomNum) {
+            if (guess > randomNum) {
+                System.out.println("Too high, try again.");
+                guess = kb.nextInt();
+            } else if (guess < randomNum) {
+                System.out.println("Too Low, try again.");
+                guess = kb.nextInt();
+            }
+        }
+        System.out.println("Congratulations! You've guessed the correct number.");
+    }
+}
